01de908c555d038ae8fd5614485a98466ff4a8a6
Anthony Bargnesi <abargnesi@gmail.com>
2013-11-12 14:15:44 -0500

    read RAGEL_OPTS to determine how to build
    quotes for better filename recognition

--- scripts/ragel-build.sh
+++ scripts/ragel-build.sh
@@ -2,14 +2,22 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 cd "${DIR}" || exit 1
 
+# must have RAGEL_OPTS set
+if [ -z "$RAGEL_OPTS" ]; then
+    echo "RAGEL_OPTS not set.  Not sure how to build." 1>&2
+    exit 1
+fi
+
+# loop compile
 inotifywait -r -m -e modify . | while read line; do
     RET=($line)
-    FILE=${RET[2]}
-    case $FILE in
+    FILE="${RET[@]:2}"
+    case "$FILE" in
         *.rl)
-            NAME=${FILE%.rl}
-            echo "building $NAME ($FILE)"
-            COMPILE_OUT=$(ragel -p -R $FILE 2>&1)
+            NAME="${FILE%.rl}"
+            echo "building '$NAME' ($FILE); RAGEL_OPTS = $RAGEL_OPTS"
+
+            COMPILE_OUT=$(ragel $RAGEL_OPTS "$FILE" 2>&1)
             if [ "$?" != 0 ]; then
                 notify-send -t 5000 "$COMPILE_OUT"
                 echo -e "  ragel failure:\n$COMPILE_OUT"
--- scripts/ragel-statechart.sh
+++ scripts/ragel-statechart.sh
@@ -2,21 +2,29 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 cd "${DIR}" || exit 1
 
+# must have RAGEL_OPTS set
+if [ -z "$RAGEL_OPTS" ]; then
+    echo "RAGEL_OPTS not set.  Not sure how to build." 1>&2
+    exit 1
+fi
+
+# loop compile + state chart
 inotifywait -r -m -e modify . | while read line; do
     RET=($line)
-    FILE=${RET[2]}
-    case $FILE in
+    FILE="${RET[@]:2}"
+    case "$FILE" in
         *.rl)
-            NAME=${FILE%.rl}
-            echo "building $NAME ($FILE)"
-            COMPILE_OUT=$(ragel -R $FILE 2>&1)
+            NAME="${FILE%.rl}"
+            echo "building '$NAME' ($FILE); RAGEL_OPTS = $RAGEL_OPTS"
+
+            COMPILE_OUT=$(ragel $RAGEL_OPTS "$FILE" 2>&1)
             if [ "$?" != 0 ]; then
                 notify-send -t 5000 "$COMPILE_OUT"
                 echo -e "  ragel failure:\n$COMPILE_OUT"
                 continue
             fi
 
-            ragel -p -R $FILE -V | dot -Tpng -o "$NAME.png"
+            ragel -p -R "$FILE" -V | dot -Tpng -o "$NAME.png"
             echo "  generated state chart ($NAME.png)"
             if [ "$?" == 0 ]; then
                 HAS_FEH=$(ps aux | grep "feh.*$NAME.png" | grep -v grep -c)
